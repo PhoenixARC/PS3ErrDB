7c2998fb856ec6526f6f2ba49ab58a1cdfc4493d
PhoenixARC <46140834+PhoenixARC@users.noreply.github.com>
2020-04-16 16:52:31 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace PS3ErrDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup9 = new System.Windows.Forms.ListViewGroup("Identified", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup10 = new System.Windows.Forms.ListViewGroup("Unidentified", System.Windows.Forms.HorizontalAlignment.Left);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Severity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ErrDesc = new System.Windows.Forms.Label();
+            this.ErrType = new System.Windows.Forms.Label();
+            this.ErrSev = new System.Windows.Forms.Label();
+            this.ErrCode = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.UpdateTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(846, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.Severity,
+            this.Type});
+            listViewGroup9.Header = "Identified";
+            listViewGroup9.Name = "Identified";
+            listViewGroup10.Header = "Unidentified";
+            listViewGroup10.Name = "Unidentified";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup9,
+            listViewGroup10});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 42);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(511, 596);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // id
+            // 
+            this.id.Text = "Error Code";
+            this.id.Width = 172;
+            // 
+            // Severity
+            // 
+            this.Severity.Text = "Severity";
+            this.Severity.Width = 74;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Error Type";
+            this.Type.Width = 261;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ErrDesc);
+            this.panel1.Controls.Add(this.ErrType);
+            this.panel1.Controls.Add(this.ErrSev);
+            this.panel1.Controls.Add(this.ErrCode);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(530, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 596);
+            this.panel1.TabIndex = 4;
+            // 
+            // ErrDesc
+            // 
+            this.ErrDesc.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrDesc.Location = new System.Drawing.Point(112, 111);
+            this.ErrDesc.Name = "ErrDesc";
+            this.ErrDesc.Size = new System.Drawing.Size(276, 472);
+            this.ErrDesc.TabIndex = 7;
+            this.ErrDesc.Text = " ";
+            // 
+            // ErrType
+            // 
+            this.ErrType.AutoSize = true;
+            this.ErrType.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrType.Location = new System.Drawing.Point(112, 80);
+            this.ErrType.Name = "ErrType";
+            this.ErrType.Size = new System.Drawing.Size(14, 24);
+            this.ErrType.TabIndex = 6;
+            this.ErrType.Text = " ";
+            // 
+            // ErrSev
+            // 
+            this.ErrSev.AutoSize = true;
+            this.ErrSev.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrSev.Location = new System.Drawing.Point(112, 47);
+            this.ErrSev.Name = "ErrSev";
+            this.ErrSev.Size = new System.Drawing.Size(14, 24);
+            this.ErrSev.TabIndex = 5;
+            this.ErrSev.Text = " ";
+            // 
+            // ErrCode
+            // 
+            this.ErrCode.AutoSize = true;
+            this.ErrCode.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrCode.Location = new System.Drawing.Point(112, 15);
+            this.ErrCode.Name = "ErrCode";
+            this.ErrCode.Size = new System.Drawing.Size(14, 24);
+            this.ErrCode.TabIndex = 4;
+            this.ErrCode.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Error Type:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Error Severity:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Error Code:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(530, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Last Update:";
+            // 
+            // UpdateTime
+            // 
+            this.UpdateTime.AutoSize = true;
+            this.UpdateTime.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateTime.Location = new System.Drawing.Point(604, 9);
+            this.UpdateTime.Name = "UpdateTime";
+            this.UpdateTime.Size = new System.Drawing.Size(34, 24);
+            this.UpdateTime.TabIndex = 5;
+            this.UpdateTime.Text = "      ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 696);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UpdateTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader Severity;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ErrDesc;
+        private System.Windows.Forms.Label ErrType;
+        private System.Windows.Forms.Label ErrSev;
+        private System.Windows.Forms.Label ErrCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label UpdateTime;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS3ErrDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.File.WriteAllText(Environment.CurrentDirectory + "\\time.txt", DateTime.Now.Date.ToLocalTime().ToString().Replace("00:00:00", "") + " at " + DateTime.Now.TimeOfDay.ToString());
+                var client = new System.Net.WebClient();
+                client.DownloadFile("https://pastebin.com/raw/EJCyPdkt", "db.txt");
+                UpdateTime.Text = System.IO.File.ReadAllText(Environment.CurrentDirectory + "\\time.txt");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error: \n Could not load server, loading \n from local database!","");
+            }
+            string line = System.IO.File.ReadAllText(Environment.CurrentDirectory + "\\db.txt");
+            string[] seperated = line.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            foreach (string item in seperated)
+            {
+                try
+                {
+                    string[] items = item.Split('	');
+                    string name = items[0];
+                    string sev = items[1];
+                    string type = items[2];
+                    string desc = items[3];
+                    ListViewItem item1;
+                    item1 = new ListViewItem(name);
+                    item1.Group = listView1.Groups[0];
+                    item1.SubItems.Add(sev);
+                    item1.SubItems.Add(type);
+                    item1.SubItems.Add(desc);
+                    listView1.Items.Add(item1);
+                }
+                catch (Exception err)
+                {
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            try
+            {
+                System.IO.File.WriteAllText(Environment.CurrentDirectory + "\\time.txt", DateTime.Now.Date.ToLocalTime().ToString().Replace("00:00:00", "") + " at " + DateTime.Now.TimeOfDay.ToString());
+                var client = new System.Net.WebClient();
+                client.DownloadFile("https://pastebin.com/raw/EJCyPdkt", "db.txt");
+                UpdateTime.Text = System.IO.File.ReadAllText(Environment.CurrentDirectory + "\\time.txt");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error: \n Could not load server, loading \n from local database!", "");
+            }
+            string line = System.IO.File.ReadAllText(Environment.CurrentDirectory + "\\db.txt");
+            string[] seperated = line.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            foreach (string item in seperated)
+            {
+                try
+                {
+                    string[] items = item.Split('	');
+                    string name = items[0];
+                    string sev = items[1];
+                    string type = items[2];
+                    string desc = items[3];
+                    ListViewItem item1;
+                    item1 = new ListViewItem(name);
+                    item1.Group = listView1.Groups[0];
+                    item1.SubItems.Add(sev);
+                    item1.SubItems.Add(type);
+                    item1.SubItems.Add(desc);
+                    listView1.Items.Add(item1);
+                }
+                catch (Exception err)
+                {
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ErrCode.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                ErrSev.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                ErrType.Text = listView1.SelectedItems[0].SubItems[2].Text;
+                ErrDesc.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string line = System.IO.File.ReadAllText(Environment.CurrentDirectory + "\\db.txt");
+            string[] seperated = line.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                foreach (string item in seperated)
+                {
+                    try
+                    {
+                        string[] items = item.Split('	');
+                        string name = items[0];
+                        string sev = items[1];
+                        string type = items[2];
+                        string desc = items[3];
+                        ListViewItem item1;
+                        item1 = new ListViewItem(name);
+                        item1.Group = listView1.Groups[0];
+                        item1.SubItems.Add(sev);
+                        item1.SubItems.Add(type);
+                        item1.SubItems.Add(desc);
+                        listView1.Items.Add(item1);
+                    }
+                    catch (Exception err)
+                    {
+                    }
+                }
+            }
+            else
+            {
+                foreach (string item in seperated)
+                {
+                    try
+                    {
+                        string[] items = item.Split('	');
+                        string name = items[0];
+                        string sev = items[1];
+                        string type = items[2];
+                        string desc = items[3];
+                        ListViewItem item1;
+                        item1 = new ListViewItem(name);
+                        item1.Group = listView1.Groups[0];
+                        item1.SubItems.Add(sev);
+                        item1.SubItems.Add(type);
+                        item1.SubItems.Add(desc);
+                        if (items[0].Contains(textBox1.Text))
+                        {
+                            listView1.Items.Add(item1);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
